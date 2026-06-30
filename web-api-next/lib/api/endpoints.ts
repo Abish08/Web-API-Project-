c3accfea00900acf80cef6870245c536ace8f6ca
Abish <abishkhanal29@gmail.com>
2026-06-30 20:48:30 +0545

    Sprint 4

--- web-api-next/lib/api/endpoints.ts
+++ web-api-next/lib/api/endpoints.ts
@@ -5,5 +5,14 @@ export const API = {
     WHOAMI: "/api/v1/auth/whoami",
     UPDATE_PROFILE: "/api/v1/auth/update",
     CHANGE_PASSWORD: "/api/v1/auth/change-password",
+  },
+  ADMIN: {
+    USERS: {
+      GET_ALL: "/api/v1/admin/users",
+      GET_BY_ID: (id: string) => `/api/v1/admin/users/${id}`,
+      CREATE: "/api/v1/admin/users",
+      UPDATE: (id: string) => `/api/v1/admin/users/${id}`,
+      DELETE: (id: string) => `/api/v1/admin/users/${id}`,
+    }
   }
-};
+}
